e7080980264d3e25d3f50ac1cebedb05b14dd4f2
bensonbaragu <bensonbaragu@gmail.com>
2024-05-01 17:54:21 +0300

    Backend docker file updated

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@bbcluster.w33tl4z.mongodb.net/?retryWrites=true&w=majority&appName=bbcluster',
+    development: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@bbcluster.w33tl4z.mongodb.net/?retryWrites=true&w=majority&appName=bbcluster',
+    test: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@bbcluster.w33tl4z.mongodb.net/?retryWrites=true&w=majority&appName=bbcluster',
+}
+module.exports = config;
